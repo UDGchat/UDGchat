645f647bfb7da768b8c9414f1a51803380ee22df
UDGchat <angel.barrera1633@alumnos.udg.mx>
2022-11-06 12:02:57 -0600

    Added mongoose, cors and dotenv to backend, and tested successful connection to MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -1,6 +1,22 @@
+const cors = require('cors');
 const express = require('express');
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
 const app = express()
+const port = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, {})
+const connection = mongoose.connection
+connection.once('open', () =>
+    console.log('Connection to MongoDB successful')
+)
 
+app.use(cors())
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
 })
